b804fe1e2ef19ee3f8b967d4309cfe5769609666
unknown <beidi.beer@gmail.com>
2023-09-26 22:26:41 +0100

    fist commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,8 +5,18 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
+
 const Nav = () => {
   const isUserLoggedin = true;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    }
+    setProviders();
+  },[])
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
